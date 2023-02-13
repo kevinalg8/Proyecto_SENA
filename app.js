8c6206747276f65c05e3142100cb58fd5083c6fc
Julian <jarboleda030@misena.edu.co>
2023-02-13 16:34:49 -0500

    avances

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+//app.use(express.static('public'))
+
+app.use("/resources", express.static("public"));
+app.use("/resources", express.static(__dirname + "/public"));
+//4- Estableciendo el motor de plantillas
+app.set('view engine','ejs');
+
+//Para poder capturar los datos del formulario (sin urlencoded nos devuelve "undefined")
+app.use(express.urlencoded({extended:false}));
+//además le decimos a express que vamos a usar json
+app.use(express.json());
+
+
+ //Establecemos las rutas para las VISTAS usando un archivo aparte (router.js) y la clase Router()
+//app.use('/', require('./router'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(8080, ()=>{
+    console.log('SERVER corriendo en http://localhost:8080');
+});
